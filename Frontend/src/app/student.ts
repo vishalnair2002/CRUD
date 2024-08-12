b4c0a5ba816136cbda9768dad07f03f9acada25c
Vishal Nair <148544787+vishalnair2002@users.noreply.github.com>
2024-08-12 09:36:15 +0400

    Update student.ts

--- Frontend/src/app/student.ts
+++ Frontend/src/app/student.ts
@@ -1,10 +1,11 @@
 export interface Student {
     id: number;
     name: string;
-    photo: string;
+    photo: File;
     phone: string;
     email: string;
     studentId: string;
     major: string;
     year: number;
+    grade: string;
   }
